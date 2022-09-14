827897d880f5df6e94408a9d61284b3ca3690fb9
이승현 <dltmdgus741@gmail.com>
2022-09-14 19:01:31 +0900

    승현: 게임 맵 최단거리

--- programmers/코딩테스트_고득점_Kit/깊이_너비 우선탐색(DFS_BFS)/게임 맵 최단거리/승현.java
+++ programmers/코딩테스트_고득점_Kit/깊이_너비 우선탐색(DFS_BFS)/게임 맵 최단거리/승현.java
@@ -1,58 +1,55 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.awt.Point;
+import java.util.*;
 
 class Solution {
+    public int solution(int[][] maps) {
+        int n = maps.length, m = maps[0].length;
+        int[] dirX = new int[]{1, -1, 0, 0};
+        int[] dirY = new int[]{0, 0, 1, -1};
 
-    public static int solution(int[][] maps) {
-        int X = maps[0].length;
-        int Y = maps.length;
-        boolean[][] visited = new boolean[Y][X];
-        Queue<Point> q = new LinkedList<Point>();
-        int x = 0;
-        int y = 0;
-        int size = 0;
-        int cnt = 0;
-        Point p = new Point();
-        q.add(new Point(Y-1,X-1));
+        int answer = n * m + 1;
+        int[][] visit = new int[n][m];
 
-        while(q.isEmpty()==false) {
-            size = q.size();
-            cnt++;
-            for(int i=0;i<size;i++)
-            {
-                p = q.peek();
-                x = p.y;
-                y = p.x;
-                q.remove();
-                if(visited[y][x]==true)
-                    continue;
-                maps[y][x] = 0;
-                visited[y][x] = true;
-                if(x==0 && y==0) {
-                    return cnt;
-                }
-
-                if(x-1>-1 && maps[y][x-1]==1) { //왼쪽 한칸
-                    q.add(new Point(y,x-1));
-                }
-
-                if(x+1<X && maps[y][x+1]==1) { //오른쪽 한칸
-                    q.add(new Point(y,x+1));
-                }
-
-                if(y-1>-1 && maps[y-1][x]==1) { //위쪽 한칸
-                    q.add(new Point(y-1,x));
-                }
-
-                if(y+1<Y && maps[y+1][x]==1) { //아래쪽 한칸
-                    q.add(new Point(y+1,x));
-                }
-
-            }
-
+        for (int[] v: visit) {
+            Arrays.fill(v, -1);
         }
 
-        return -1;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(0, 0));
+        visit[0][0] = 1;
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int x = node.x, y = node.y;
+
+            if (x == m-1 && y == n-1) {
+                answer = Math.min(answer, visit[y][x]);
+                continue;
+            }
+
+            for (int i=0; i<4; i++) {
+                int newX = x + dirX[i];
+                int newY = y + dirY[i];
+
+                if (newX < 0 || newX >= m || newY < 0 || newY >= n) {
+                    continue;
+                }
+
+                if (visit[newY][newX] == -1 && maps[newY][newX] == 1) {
+                    q.offer(new Node(newX, newY));
+                    visit[newY][newX] = visit[y][x] + 1;
+                }
+            }
+        }
+
+        return answer == n * m + 1 ? -1 : answer;
+    }
+
+    class Node {
+        int x, y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
